3921808ccbedc24514c4b9033fbab77744d7f10c
Daniele <daniele.ninicato@gmail.com>
2023-12-28 12:19:01 +0100

    logging added to test

--- src/test/java/com/example/springbootkubernetes/SpringBootKubernetesApplicationTests.java
+++ src/test/java/com/example/springbootkubernetes/SpringBootKubernetesApplicationTests.java
@@ -1,13 +1,15 @@
 package com.example.springbootkubernetes;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Slf4j
 @SpringBootTest
 class SpringBootKubernetesApplicationTests {
 
 	@Test
 	void contextLoads() {
+		log.info("Loading context...");
 	}
-
 }
