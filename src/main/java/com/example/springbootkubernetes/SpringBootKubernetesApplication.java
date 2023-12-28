e04c30e23ba36fff00dce777695b14f58ac8a6bd
Daniele <daniele.ninicato@gmail.com>
2023-12-28 11:47:47 +0100

    comment removed

--- src/main/java/com/example/springbootkubernetes/SpringBootKubernetesApplication.java
+++ src/main/java/com/example/springbootkubernetes/SpringBootKubernetesApplication.java
@@ -6,9 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootKubernetesApplication {
 
-	// I created this empty spring-boot project just for testing purpose.
-	// You can check if the app is 'working correctly' through the actuator health endpoint (...actuator/health)
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootKubernetesApplication.class, args);
 	}
